2763e4ff17a1bcf32be39f94a0fa4e6949f935da
SkylynxX <meshcheriakova.elizaveta@gmail.com>
2022-04-12 01:12:55 +0300

    плавное открывание const

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
-let popupHeadForm = document.querySelector('.popup');
-let popupFormName = document.querySelector ('.popup__input_type_name');
-let popupFormInfo = document.querySelector ('.popup__input_type_info');
-let popupShowButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__form');
-let nameTitle = document.querySelector ('.profile__title');
-let infoSubtitle = document.querySelector ('.profile__subtitle');
-let popupNewItemForm = document.querySelector('.popup_new-item');
-let popupPlusShowButton = document.querySelector('.profile__add-button');
-let popupPlusCloseButton = document.querySelector('.popup__close_new-item');
-let templateElemet = document.querySelector('.template__element').content;
-let elementList = document.querySelector('.elements');
-let popupNIFormName = document.querySelector('.popup__input_new-item_name');
-let popupNIFormLink = document.querySelector('.popup__input_new-item_link');
-let popupNIForm = document.querySelector('.popup__form_new-item');
-let popupImage = document.querySelector('.popup-image');
-let popupImgClose = document.querySelector('.popup-image__close');
+const popupHeadForm = document.querySelector('.popup');
+const popupFormName = document.querySelector ('.popup__input_type_name');
+const popupFormInfo = document.querySelector ('.popup__input_type_info');
+const popupShowButton = document.querySelector('.profile__edit-button');
+const popupCloseButton = document.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__form');
+const nameTitle = document.querySelector ('.profile__title');
+const infoSubtitle = document.querySelector ('.profile__subtitle');
+const popupNewItemForm = document.querySelector('.popup_new-item');
+const popupPlusShowButton = document.querySelector('.profile__add-button');
+const popupPlusCloseButton = document.querySelector('.popup__close_new-item');
+const templateElemet = document.querySelector('.template__element').content;
+const elementList = document.querySelector('.elements');
+const popupNIFormName = document.querySelector('.popup__input_new-item_name');
+const popupNIFormLink = document.querySelector('.popup__input_new-item_link');
+const popupNIForm = document.querySelector('.popup__form_new-item');
+const popupImage = document.querySelector('.popup-image');
+const popupImgClose = document.querySelector('.popup-image__close');
 
 const initialCards = [
   {
@@ -81,7 +81,7 @@ function closePopupPlus () {
 
 //функция добавления 6-ти карточек
 initialCards.forEach(function (item){
-  let elementContent = templateElemet.querySelector('.element').cloneNode(true);
+  const elementContent = templateElemet.querySelector('.element').cloneNode(true);
   elementContent.querySelector('.element__image').src = item.link;
   elementContent.querySelector('.element__group-text').textContent = item.name;
   elementList.prepend(elementContent);
@@ -102,7 +102,7 @@ function addPushLikeListener(item){
 //функция добавления слушателя удаления карточек
 function addPushRemoveListener(item){
   item.addEventListener('click', function (){
-    let listItem = item.closest('.element');
+    const listItem = item.closest('.element');
     listItem.remove();
   })
 }
@@ -120,7 +120,7 @@ function addPushPictureListener(item){
 //функция добавления карточек через форму в попапе new-item
 function addCard (evt) {
   evt.preventDefault();
-  let elementContent = templateElemet.querySelector('.element').cloneNode(true);
+  const elementContent = templateElemet.querySelector('.element').cloneNode(true);
   elementContent.querySelector('.element__image').src = popupNIFormLink.value;
   elementContent.querySelector('.element__image').alt = popupNIFormName.value;
   elementContent.querySelector('.element__group-text').textContent = popupNIFormName.value;
@@ -149,13 +149,13 @@ function closePopupImg () {
 
 
 //поиск кнопок должен быть выполнен после создания карточек
-let likeButtons = document.querySelectorAll('.element__group-button');
+const likeButtons = document.querySelectorAll('.element__group-button');
 likeButtons.forEach(addPushLikeListener);
 //поиск кнопок должен быть выполнен после создания карточек
-let trashButton = document.querySelectorAll('.element__trash');
+const trashButton = document.querySelectorAll('.element__trash');
 trashButton.forEach(addPushRemoveListener);
 //поиск картинок должен быть выполнен после создания карточек
-let cardImgList = document.querySelectorAll('.element__image');
+const cardImgList = document.querySelectorAll('.element__image');
 cardImgList.forEach(addPushPictureListener);
 
 
